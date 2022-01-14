da5a43be729e9a9766aca5cffcf15352085e3185
jeong98CH <jchul1998@naver.com>
2022-01-14 15:57:11 +0900

    Add project files.

---
+++ Exercise_수정본/Exercise_9/Archer.cpp
@@ -0,0 +1,32 @@
+#include "Archer.h"
+#include "Pet.h"
+
+Archer::Archer(Pet* pet) : _pet(pet)
+{
+	
+}
+
+Archer::Archer(int hp) : Player(hp), _pet(nullptr)
+{
+}
+
+Archer::~Archer()
+{
+	if(_pet != nullptr)
+	{
+		delete _pet;
+	}
+	
+}
+
+void Archer::AddHp(int value)
+{
+	Player::AddHp(value);
+
+	// Á×¾úÀ¸¸é Æêµµ ³¯¸°´Ù
+	if (IsDead())
+	{
+		delete _pet;
+		_pet = nullptr;
+	}
+}
