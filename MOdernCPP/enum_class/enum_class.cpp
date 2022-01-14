fe73c998941c90c03d513aaa1f0f9a2d8cd7f7af
jeong98CH <jchul1998@naver.com>
2022-01-14 17:14:38 +0900

    enum_class

---
+++ MOdernCPP/enum_class/enum_class.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+
+enum PlayerType :  char //data type지정
+{
+	PT_Knight,
+	PT_Archer
+};
+// 기본적으로 int 로 사용된다.
+
+
+enum class ObjectType
+{
+	Player,
+	Knight,
+	Archer,
+};
+
+enum class ObjectType2
+{
+	Player,
+	Knight,
+	Archer,
+};
+
+
+#if 1
+int main()
+{
+	double Val1 = PT_Knight;
+	double Val2 = static_cast<double>(ObjectType::Knight);
+
+	size_t bitFlag;
+	bitFlag << PT_Knight;
+	bitFlag << static_cast<int>(ObjectType::Player);
+
+
+
+}
+#endif // 1
