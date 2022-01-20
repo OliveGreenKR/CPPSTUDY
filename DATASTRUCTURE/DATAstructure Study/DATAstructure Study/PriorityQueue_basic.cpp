2db7da8eed80b098cacd04623a90c6e478a2fd82
jeong98CH <jchul1998@naver.com>
2022-01-20 15:24:44 +0900

    ver0.2 teacher's first ver

--- DATASTRUCTURE/DATAstructure Study/DATAstructure Study/PriorityQueue_basic.cpp
+++ DATASTRUCTURE/DATAstructure Study/DATAstructure Study/PriorityQueue_basic.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-template<typename T>
+template<typename T, class Container =  vector<T>>
 class Priority_Queue
 {
 public:
@@ -17,8 +17,57 @@ public:
 
 	void push(const T& data)
 	{
+		//우선 힙 구조 부터 맞춰줌
 		_container.push_back(data);
-		Arrange();
+
+		//도장깨기_정렬
+		int now = static_cast<int>(_container.size()) - 1;
+		while (now > 0)
+		{
+			int parent = (now - 1) / 2;
+
+			if (_container[parent] > _container[now])
+				break;
+
+			::swap(_container[parent], _container[now]);
+			now = parent;
+		}
+		/*Arrange();*/
+
+
+	}
+
+	void pop()
+	{
+		_container[0] = _container.back();
+		_container.pop_back();
+
+		//정렬
+		int now = 0;
+		while (true)
+		{
+			int child1 = 2*now + 1;
+			int child2 = 2*now + 2;
+
+			if (child1 >= _container.size())
+				break;
+			int next = now;
+
+			//compare to left
+			if (_container[next] < _container[child1])
+				next = child1;
+
+			//둘 중 승자를 오른쪽과 비교
+			if (child2 < _container.size() && _container[next] < _container[child2])
+				next = child2;
+			
+			if (next == now)
+				break;
+
+			::swap(_container[now], _container[next]);
+			now = next;
+		}
+		/*Arrange();*/
 	}
 
 	bool empty()
@@ -31,19 +80,9 @@ public:
 		return _container[0];
 	}
 
-	void pop()
-	{
-		int size = _container.size();
-		int top = _container[0];
-
-		swap(_container[0], _container[size - 1]);
-		_container.pop_back();
-		Arrange();
-	}
-
-
 private:
-	vector<T> _container;
+	Container _container;
+
 	void Arrange()
 	{
 		int size = _container.size();
@@ -60,6 +99,7 @@ int main()
 {
 	/*priority_queue<int> pq;*/
 	Priority_Queue<int> pq;
+
 
 	pq.push(100);
 	pq.push(500);
