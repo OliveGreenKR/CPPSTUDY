3d810b3e51f292ea2abb59f31df9ab71761f5532
jeong98CH <jchul1998@naver.com>
2022-01-20 19:00:08 +0900

    my BS

--- DATASTRUCTURE/DATAstructure Study/DATAstructure Study/Binary Search Tree.cpp
+++ DATASTRUCTURE/DATAstructure Study/DATAstructure Study/Binary Search Tree.cpp
@@ -7,7 +7,46 @@
 using namespace std;
 
 
+vector<int> numbers;
+
+
+void BinarySearch(int N)
+{
+	int last = static_cast<int>(numbers.size());
+	int start = 0;
+
+	while(true)
+	{
+		int middle = (last + start)/ 2;
+
+		if (numbers[middle] == N)
+		{
+			cout << "found!" << endl;
+			break;
+		}
+			
+		if (middle <= 0 || middle >= numbers.size()-1)
+		{
+			cout << "Not found!" << endl;
+			break;
+		}
+
+		if (numbers[middle] < N)
+		{
+			start = middle;
+			continue;
+		}
+
+		last = middle;
+
+	}
+}
+
 #if 1
 int main()
-{}
+{
+	numbers = vector<int>{ 1, 8, 15, 23, 32, 44, 56, 63, 81 ,99 };
+
+	BinarySearch(82);
+}
 #endif // 1
