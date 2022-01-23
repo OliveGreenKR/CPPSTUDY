574c9dfc137e8f0194d878c9d09dd651446f67c3
jeong98CH <jchul1998@naver.com>
2022-01-23 20:52:35 +0900

    basic sort

--- DATASTRUCTURE/DATAstructure Study/DATAstructure Study/정렬_버블,선택,삽입.cpp
+++ DATASTRUCTURE/DATAstructure Study/DATAstructure Study/정렬_버블,선택,삽입.cpp
@@ -1,3 +1,4 @@
+#if 0
 #include <iostream>
 #include <vector>
 #include <list>
@@ -40,22 +41,59 @@ void PrintVec(const vector<int>& v)
 {
 	const int n = static_cast<int>(v.size());
 
-	for (int i = 0; i < n - 1; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << v[i] << endl;
 	}
 }
 
+void SelectionSort(vector<int>& v)
+{
+	int n = static_cast<int>(v.size());
+	int cnt = 0;
+	bool fn = false;
 
+	for (int i = 0; i < n - 1; i++)
+	{
+		int small = INT_MAX;
+		for (int j = i; j < n; j++)
+		{
+			if (v[j] < small)
+			{
+				small = v[j];
+				::swap(v[j], v[i]);
+			}
 
+		}
+	}
+}
+
+void InsertSort(vector<int>& v)
+{
+	const int n = static_cast<int>(v.size());
+	for (int i = 1; i < n; i++)
+	{
+		int insertData = v[i];
+		int j;
+		for (j = i - 1; j >= 0; j--) //정렬안된 배열
+		{
+			if (v[j] > insertData)
+			{
+				v[j + 1] = v[i];
+			}
+			else
+				break;
+		}
+		v[j + 1] = insertData;
+	}
+
+}
 int main()
 {
-	vector<int> v = { 1,4,6,10,200,39 };
-	
+	vector<int> v = { 100, 1,4,6,10,39 };
+
 	//BubbleSort(v);
-
+	SelectionSort(v);
 	PrintVec(v);
-
-
-	
 }
+#endif // 0
