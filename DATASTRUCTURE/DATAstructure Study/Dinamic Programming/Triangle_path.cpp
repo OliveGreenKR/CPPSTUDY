cd686d0a104ed63751599123c4bf40be21abbf51
jeong98CH <jchul1998@naver.com>
2022-01-26 16:03:07 +0900

    path update

--- DATASTRUCTURE/DATAstructure Study/Dinamic Programming/Triangle_path.cpp
+++ DATASTRUCTURE/DATAstructure Study/Dinamic Programming/Triangle_path.cpp
@@ -9,7 +9,7 @@ using namespace std;
 * 문제 : 삼각 경로
 * (0,0)에서ㅏ 시작해서  아래 or 아래우측으로 이동가능
 * 만나는 숫자는 모두 더한다.
-* 더한 숫자가 최대가 되는 경로의 합은?
+* 더한 숫자가 최대가 되는 경로의 합은? 그렇다면 경로는?
 * 
 * 6
 * 1 2
@@ -39,18 +39,36 @@ vector<vector<int>> board=
 };
 
 vector<vector<int>> cache;
+vector<vector<int>> nextX;
 
-int Path( int y, int x)
+int Path( int y=0, int x=0)
 {
+	//기저상황
 	int N = board.size();
 	if (x > N-1 || y > N-1)
 		return 0;
 
+	//캐시확인
 	if (cache[y][x] != 0)
 		return cache[y][x];
 
+	//적용
 	int sum = board[y][x];
-	sum = sum + max(Path(y + 1, x), Path(y + 1, x + 1));
+	int nextRight = Path(y + 1, x);
+	int nextRight2 = Path(y + 1, x + 1);
+
+	if (nextRight > nextRight2)
+	{
+		sum += nextRight;
+		nextX[y][x] = x;
+	}
+	else
+	{
+		sum += nextRight2;
+		nextX[y][x] = x+1;
+	}
+
+	//sum = sum + max(Path(y + 1, x), Path(y + 1, x + 1));
 	cache[y][x] = sum;
 
 	return sum;
@@ -60,8 +78,19 @@ int main()
 {
 	int N = board.size();
 	cache = vector<vector<int>>(N, vector<int>(N, 0));
+	nextX = vector<vector<int>>(N, vector<int>(N));
 
-	int ans = Path(0, 0);
+	int ans = Path();
+	
+	int y = 0, x = 0;
+
+	while (y < N)
+	{
+		cout << board[y][x] << " ->";
+		
+		x = nextX[y][x];
+		y++;
+	}
 }
 
 #endif // 1
